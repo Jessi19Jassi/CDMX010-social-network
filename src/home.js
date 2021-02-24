291280995253c4e84d95515926fb4f60ed77a707
dany correo <danycorreos96@gmail.com>
2021-02-23 22:21:12 -0600

    este commit contiene las modificaiones de css en home y renombres de modulos

--- src/home.js
+++ src/home.js
@@ -2,21 +2,35 @@ import { onNavigate } from './routes.js'
 
 export const home = (target) => {
     const html = `
+    <div class="displayOne">
+    <div class="title">
     <h1>Visage D' Amour<h1>
+    <img src="../imagenes/visage4.png">
+    </div>
+    <div class="singUp">
+    <button class="buttonF">Iniciar sesion con Facebook <i class="fab fa-facebook"></i></button>
+    <br>
+    <button class="buttonG">Iniciar sesion con Google <i class="fab fa-google"></i></button>
+    <br>
+    <p>O</p>
+    <br>
     <input id="name" type="text" placeholder="usuario">
     <br>
     <input id="password" type="text" placeholder="contraseña">
     <br>
-    <button id="ingreso">Ingresar</button>
-        
+    <button class="bottonSingUp" id="ingreso">Ingresar</button>
+    <br>
+        <p>¿No tienes cuenta?</p>
             <ul class="navbar-item"><a href="#" id="aboutLink">Registrate!!</a></ul>
+            </div>
+            </div>
     `
 
     target.innerHTML = html
 
     document.getElementById('aboutLink').addEventListener('click', (e) => {
         e.preventDefault()
-        onNavigate('/about')
+        onNavigate('/register')
     })
 
     /*document.getElementById('homeLink').addEventListener('click', (e) => {
@@ -26,7 +40,7 @@ export const home = (target) => {
 
     document.getElementById('ingreso').addEventListener('click', (e) => {
         e.preventDefault()
-        onNavigate('/contact')
+        onNavigate('/singUp')
     })
 }
 
---
+++ src/singUp.js
@@ -0,0 +1,13 @@
+export const singUp = (target) =>{
+    const html=
+`<h1 class="texto">Hola! aqui mostraremos nuestras publicaciones y comentarios</h1>
+<input type="text" placeholder="Escribe aquí tu post">
+<br>
+<button>publicar</button>
+<br>
+<p>aqui se publicaran tus post</p>
+`
+
+target.innerHTML= html
+
+ }
